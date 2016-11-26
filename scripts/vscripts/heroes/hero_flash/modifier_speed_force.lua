d1de3373c1100dcc4475064629e666083c1597e2
anilbanarsee <anil.banarsee@googlemail.com>
2016-11-26 20:35:36 +0000

    Added speed force skeleton (no modifier yet)

---
+++ scripts/vscripts/heroes/hero_flash/modifier_speed_force.lua
@@ -0,0 +1,24 @@
+modifier_speed_force = class({})
+
+function modifier_speed_force:DeclareFunctions()
+	
+		local funcs_array = {
+			MODIFIER_PROPERTY_MOVESPEED_ABSOLUTE ,
+			MODIFIER_PROPERTY_MOVESPEED_MAX,
+			MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT
+			}
+		
+		return funcs_array
+end
+
+function modifier_speed_force:GetModifierMoveSpeed_Absolute(params)
+	return 1000;
+end
+
+function modifier_speed_force:GetModifierMoveSpeed_Max(params)
+	return 1000;
+end
+
+function modifier_speed_force:GetModifierAttackSpeedBonus_Constant(params)
+	return 100;
+end
